22a1aeeb231a1e2d0379b43512a7001629ef5cca
ardailhan <ardailhan_@outlook.com>
2023-09-18 21:51:33 +0300

    Add project files.

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Arda\\SQLEXPRESS;initial catalog=DesignPattern; integrated security=true;");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
