509ee9e744426f2812d56f3739bffdda2e45e940
ardailhan <ardailhan_@outlook.com>
2023-09-19 17:01:59 +0300

    Cqrs Pattern

---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Arda\\SQLEXPRESS;initial catalog=DesignPattern2; integrated security=true;");
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
