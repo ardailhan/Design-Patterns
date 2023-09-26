d1a391a16b8a41315b80923678ddfbc71e24d966
ardailhan <ardailhan_@outlook.com>
2023-09-26 11:11:32 +0300

    Mediator Design Pattern lesson done

---
+++ MediatorDesignPattern/DesignPattern.Mediator/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using DesignPattern.Mediator.MediatorPattern.Commands;
+using DesignPattern.Mediator.MediatorPattern.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Mediator.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var values = await _mediator.Send(new GetAllProductQuery());
+            return View(values);
+        }
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var values = await _mediator.Send(new GetProductByIdQuery(id));
+            return View(values);
+        }
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _mediator.Send(new RemoveProductCommand(id));
+            return RedirectToAction("GetAllProduct");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var values = await _mediator.Send(new GetProductUpdateByIdQuery(id));
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductCommand updateProductCommand)
+        {
+            await _mediator.Send(updateProductCommand);
+            return RedirectToAction("GetAllProduct");
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(CreateProductCommand createProductCommand)
+        {
+            await _mediator.Send(createProductCommand);
+            return RedirectToAction("GetAllProduct");
+        }
+    }
+}
