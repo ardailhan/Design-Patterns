aec530a10dc95ab9d141e815136a012533b4243d
ardailhan <ardailhan_@outlook.com>
2023-09-27 13:01:40 +0300

    Facade Design Pattern

---
+++ FacadeDesignPattern/DesignPattern.Facade/FacadePattern/AddOrder.cs
@@ -0,0 +1,15 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern
+{
+    public class AddOrder
+    {
+        Context c = new();
+        public void AddNewOrder(Order order)
+        {
+            order.OrderDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.Orders.Add(order);
+            c.SaveChanges();
+        }
+    }
+}
